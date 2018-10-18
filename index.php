3f6a5466a41f16e9665f99c0246cb44d14c62538
shirotaro-jp <43288205+shirotaro-jp@users.noreply.github.com>
2018-10-19 02:28:30 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+//1.  DB接続します ログイン確認
+session_start();
+include('functions.php');
+// $unlogin = chk_ssid();
+$loginname = $_SESSION['name'];
+$pdo = db_conn();
+$userid = $_SESSION['userid'];
+
+
+//２．データ登録SQL作成
+$stmt = $pdo->prepare('SELECT * FROM '.$area_table.' WHERE user=:userid');
+$stmt->bindValue(':userid', $userid, PDO::PARAM_STR);
+$status = $stmt->execute();
+
+//３．データ表示
+$view='';
+if($status==false){
+  errorMsg($stmt);
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $view .= '<p><a href="select.php?id='.$result['id'].'">';  //更新ページへのaタグを作成
+    $view .= $result['miniarea'].'('.$result['area'].')';
+    $view .= '</a>';
+    $view .= '　';
+    $view .= '<a href="scraping.php?id='.$result['id'].'">［更新］</a></p>';  //削除用aタグを作成
+     
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -9,7 +41,9 @@
 <header>
 
 </header>
-
+<div>
+    <?=$view?>
+</div>
 
 <!-- FORM[Start] -->
 <form method="post" action="area-insert.php">
